4d170c26b697a1d1a8088a9ac2696042441bbf8d
Arun VK <arun_bolt4@yahoo.in>
2017-10-03 07:52:06 +0000

    created artifacts model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171002102023) do
+ActiveRecord::Schema.define(version: 20171003074150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "artifacts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "key"
+    t.integer  "prroject_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "artifacts", ["prroject_id"], name: "index_artifacts_on_prroject_id", using: :btree
 
   create_table "members", force: :cascade do |t|
     t.integer  "tenant_id"
@@ -100,6 +110,7 @@ ActiveRecord::Schema.define(version: 20171002102023) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "artifacts", "prrojects"
   add_foreign_key "members", "tenants"
   add_foreign_key "members", "users"
   add_foreign_key "prrojects", "tenants"
